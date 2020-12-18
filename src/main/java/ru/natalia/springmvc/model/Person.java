b875d62671f05a11d4b5171369cce99324b61f30
Natalia Gurevich <gurevich.natalia@gmail.com>
2020-12-18 18:46:29 +0100

    Validation form

--- src/main/java/ru/natalia/springmvc/model/Person.java
+++ src/main/java/ru/natalia/springmvc/model/Person.java
@@ -1,10 +1,23 @@
 package ru.natalia.springmvc.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
 
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Should be between 2 and 30 characters")
     private String name;
+
+    @Min(value = 0, message = "Age should be grater than 0")
     private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
     private String email;
 
     public Person() {
